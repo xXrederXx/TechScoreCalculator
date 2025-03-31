b5d04f92067e941c844ce4102a8f29b9ebb4af38
xXrederXx <morgenthalerthierry@gmail.com>
2025-03-31 19:09:20 +0200

    Add initial project structure and files for TechScoreCalculator

--- App.tsx
+++ App.tsx
@@ -89,7 +89,7 @@ function App(): React.JSX.Element {
             paddingHorizontal: safePadding,
             paddingBottom: safePadding,
           }}>
-          <Section title="Step One">
+          <Section title="Step One 1">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
