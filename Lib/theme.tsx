108679d3b55538fec322c86ad9211dca4bc5b5a7
xXrederXx <morgenthalerthierry@gmail.com>
2025-04-04 22:00:20 +0200

    feat: restructure theme colors for improved organization and clarity

--- Lib/theme.tsx
+++ Lib/theme.tsx
@@ -1,9 +1,41 @@
 export default {
-    colors: {
-      background: '#121212',
-      card: '#1E1E1E',
-      text: '#FFFFFF',
-      button: '#333333',
+  colors: {
+    'text': {
+      'verydark': '#5c6070',
+      'dark': '#9da1ae',
+      'normal':'#e3e4e8',
+      'light': '#fff',
+      'verylight': '#fff',
     },
+    'background': {
+      'verydark': '#000',
+      'dark': '#000',
+      'normal':'#101219',
+      'light': '#424a67',
+      'verylight': '#7d87ab',
+    },
+    'primary': {
+      'verydark': '#384977',
+      'dark': '#6178b4',
+      'normal':'#a6b3d5',
+      'light': '#ebeef6',
+      'verylight': '#fff',
+    },
+    'secondary': {
+      'verydark': '#000',
+      'dark': '#121b33',
+      'normal':'#2c427f',
+      'light': '#4f6dc2',
+      'verylight': '#9bacdc',
+    },
+    'accent': {
+      'verydark': '#112048',
+      'dark': '#25449b',
+      'normal':'#5174d5',
+      'light': '#a3b6e9',
+      'verylight': '#f5f7fd',
+    },
+   },
+   
   };
   
