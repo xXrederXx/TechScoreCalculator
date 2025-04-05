a3abcba924e5f53bc75d01573f68cacec69aacaf
xXrederXx <morgenthalerthierry@gmail.com>
2025-04-05 23:11:30 +0200

    feat: add BButton component for reusable button functionality with customizable styles

---
+++ components/BButton.tsx
@@ -0,0 +1,20 @@
+import { PreStyle } from '@/Lib/theme';
+import * as React from 'react';
+import { Text, TouchableOpacity, StyleProp, ViewStyle, TextStyle } from 'react-native';
+
+interface BButtonProps {
+    buttonStyle?: StyleProp<ViewStyle>,
+    textStyle?: StyleProp<TextStyle>,
+    buttonText?: string,
+    onClick?: () => void,
+}
+
+const BButton = (props: BButtonProps) => {
+    return (
+        <TouchableOpacity style={props.buttonStyle ? props.buttonStyle : PreStyle.button} onPress={() => { if (props.onClick) { props.onClick(); } }}>
+            <Text style={props.textStyle ? props.textStyle : PreStyle.buttonText}>{props.buttonText}</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default BButton;
